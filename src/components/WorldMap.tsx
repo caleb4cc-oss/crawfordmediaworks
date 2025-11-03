d51323227cd0cacf07e76d232d829af1a744caef
caleb4cc-oss <caleb4cc@gmail.com>
2025-11-03 23:11:04 +0000

    Updated WorldMap.tsx

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -8,9 +8,9 @@ interface ClientLocation {
 }
 
 const clientLocations: ClientLocation[] = [
-  { name: '', country: 'United Kingdom', x: 47, y: 37 },
-  { name: '', country: 'United States', x: 20, y: 40 },
-  { name: '', country: 'United Arab Emirates', x: 63, y: 43 }
+  { name: '', country: 'United Kingdom', x: 47, y: 19 },
+  { name: '', country: 'United States', x: 30, y: 25 },
+  { name: '', country: 'United Arab Emirates', x: 57, y: 33 }
 ];
 
 export default function WorldMap() {

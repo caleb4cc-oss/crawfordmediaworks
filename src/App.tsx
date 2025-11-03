f83a48ba6b82616306f67b737bd24811c4f1ad24
caleb4cc-oss <caleb4cc@gmail.com>
2025-11-03 19:14:26 +0000

    Updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,31 @@ function App() {
         <div className="wind-line wind-23"></div>
         <div className="wind-line wind-24"></div>
         <div className="wind-line wind-25"></div>
+        <div className="wind-line wind-26"></div>
+        <div className="wind-line wind-27"></div>
+        <div className="wind-line wind-28"></div>
+        <div className="wind-line wind-29"></div>
+        <div className="wind-line wind-30"></div>
+        <div className="wind-line wind-31"></div>
+        <div className="wind-line wind-32"></div>
+        <div className="wind-line wind-33"></div>
+        <div className="wind-line wind-34"></div>
+        <div className="wind-line wind-35"></div>
+        <div className="wind-line wind-36"></div>
+        <div className="wind-line wind-37"></div>
+        <div className="wind-line wind-38"></div>
+        <div className="wind-line wind-39"></div>
+        <div className="wind-line wind-40"></div>
+        <div className="wind-line wind-41"></div>
+        <div className="wind-line wind-42"></div>
+        <div className="wind-line wind-43"></div>
+        <div className="wind-line wind-44"></div>
+        <div className="wind-line wind-45"></div>
+        <div className="wind-line wind-46"></div>
+        <div className="wind-line wind-47"></div>
+        <div className="wind-line wind-48"></div>
+        <div className="wind-line wind-49"></div>
+        <div className="wind-line wind-50"></div>
       </div>
       <Hero />
       <VideoShowcase onVideoClick={setSelectedVideo} />
